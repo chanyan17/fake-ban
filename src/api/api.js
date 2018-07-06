062be53c8d5effc319aeeaa3a27dfca1a9d90608
chanyan17 <yan_chan17@163.com>
2018-07-06 18:16:49 +0800

    Add Simulation Data

---
+++ src/api/api.js
@@ -0,0 +1,17 @@
+//【获取豆瓣TOP250电影】
+https://api.douban.com/v2/movie/top250?start=0&count=10
+
+//【获取正在上映电影】
+https://api.douban.com/v2/movie/in_theaters?start=0&count=10
+
+//【 获取即将上映电影】
+https://api.douban.com/v2/movie/coming_soon?start=0&count=10
+
+//【获取具体某一电影信息】
+https://api.douban.com/v2/movie/subject/:id
+
+//【豆瓣电影北美票房榜】
+https://api.douban.com/v2/movie/us_box
+
+//【搜索】
+https://api.douban.com/v2/movie/search?q=张艺谋&?tag=喜剧
